e646fa3c8c4126c9b39e46368afccfb2766aac0a
suhail12-hacker <suhail5rahaman@gmail.com>
2020-10-20 00:52:59 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-e-commerce-8-9merpc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-e-commerce-8-9merpc)
---
+++ src/app/products.ts
@@ -0,0 +1,20 @@
+export const products = [
+  {
+    name: 'iPhone 11',
+    price: 849,
+    description: '6.1 inch Liquid Retina Display | Water and Dust resistance | 12MP Cameras | 470 grams | 256 GB Capacity | '
+  },
+  {
+    name: 'OnePlus 8T',
+    price: 610,
+    description: '5G | 12GB RAM | 256GB Storage | Aquamarine Green | Rear and Front Camera 48MP 16MP 5MP 2MP 16MP |'
+  },
+  
+  {
+    name: 'Samsung S10',
+    price: 524,
+    description: '128GB Storage | Prism Blue | 8GB RAM | 6.4 inch | Rear and Front Camera 12MP 12MP 10MP 8MP | Multi-touch Capacity |'
+  }
+];
+
+
